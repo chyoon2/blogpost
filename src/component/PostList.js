6f5461134c9bea6e93628442948f6293c8559194
Chris Yoon <chy.yoon@gmail.com>
2021-10-04 16:23:57 -0700

    added frame work for App and action and components.

---
+++ src/component/PostList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../action";
+
+class PostList extends React.Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+  render() {
+    return <div className='ui container'>Post list</div>;
+  }
+}
+
+export default connect(null, { fetchPosts: fetchPosts })(PostList);
